98a00af70e473dd18f329a45823b38d3138b9bcb
Neo <starneo@gmail.com>
2015-11-09 17:18:10 +0800

    加入baidu翻译

--- src/easy/util/TransLate.java
+++ src/easy/util/TransLate.java
@@ -5,6 +5,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.ConnectException;
 import java.net.URLEncoder;
 
+import net.sf.json.JSONObject;
 import easy.io.JFile;
 
 /**
@@ -18,7 +19,9 @@ import easy.io.JFile;
 public class TransLate
 {
 	//http://translate.google.cn/translate_a/t?client=t&text=abc&hl=zh-CN&sl=en&tl=zh-CN&multires=1&prev=btn&ssel=0&tsel=0&sc=1
-	private final static String TURL = "http://translate.google.cn/translate_a/t?client=t&text=%s&hl=zh-CN&sl=%s&tl=%s&multires=1&prev=btn&ssel=0&tsel=0&sc=1";
+	//http://translate.google.cn/translate_a/single?client=t&sl=en&tl=zh-CN&hl=zh-CN&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&source=btn&srcrom=1&ssel=0&tsel=0&kc=0&tk=90007|490943&q=test
+	//private final static String TURL = "http://translate.google.cn/translate_a/t?client=t&text=%s&hl=zh-CN&sl=%s&tl=%s&multires=1&prev=btn&ssel=0&tsel=0&sc=1";
+	private final static String TURL = "http://translate.google.cn/translate_a/single?client=t&sl=%s&tl=%s&hl=zh-CN&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&source=btn&srcrom=1&ssel=0&tsel=0&kc=0&tk=90007|490943&q=%s";
 	private final static String TTSURL = "http://translate.google.cn/translate_tts?ie=UTF-8&q=%s&tl=%s&prev=input";
 
 	
@@ -80,6 +83,7 @@ public class TransLate
 		try
 		{
 			restr = JFile.loadHttpFile(String.format(TURL, str,src,tar));
+			System.out.println(restr);
 			restr = Format.getContent(restr, "\"", "\"");
 		}
 		catch(ConnectException e)
@@ -93,14 +97,34 @@ public class TransLate
 		return restr;
 	}
 	
+	public static String baidutranslate(String str,String src,String tar) throws ConnectException, IOException
+	{
+		if (src == null)
+		{
+			src = "auto";
+		}
+		if (tar == null)
+		{
+			tar = "auto";
+		}
+		String url = String.format("http://openapi.baidu.com/public/2.0/bmt/translate?client_id=HX0knhaqSajj8tjyGeaazPbn&q=%s&from=%s&to=%s", URLEncoder.encode(str,"utf-8"),src,tar);
+		//System.out.println(url);
+		String r = JFile.loadHttpFile(url);
+		JSONObject json = JSONObject.fromObject(r);
+		//System.out.println(json);
+		return json.getJSONArray("trans_result").getJSONObject(0).getString("dst");
+	}
+	
 
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws ConnectException 
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws ConnectException, IOException
 	{
 		// en zh-CN fr
-		//System.out.println(translate("I have an apple。","en","zh-CN"));
+		System.out.println(baidutranslate("I have an apple。",null,null));
 	}
 
 }
