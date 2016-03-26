d9ce7760a39a466840cc4bdc7650c3d5051ad9e0
Neo <starneo@gmail.com>
2016-03-26 13:46:10 +0800

    fixd

--- src/dabao.sh
+++ src/dabao.sh
@@ -1,16 +1,15 @@
-cd  /Users/Neo/Documents/git/EASYCORE/web/WEB-INF/classes/
+cd  /Users/Neo/git/EASYCORE/web/WEB-INF/classes/
 jar cvf easy.jar easy/*
 cp easy.jar /Users/Neo/Documents/svn/5pv/5PVREAL/libs
 cp easy.jar /Users/Neo/Documents/svn/91duan/Project/91DUANREAL/libs
 cp easy.jar /Users/Neo/Documents/git/TaoKe/libs
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/YOUHOW/web/WEB-INF/lib
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/STOCK/web/WEB-INF/lib
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/5PVSend/libs
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/YOUHOW/web/WEB-INF/lib
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/STOCK/web/WEB-INF/lib
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/5PVSend/libs
 cp easy.jar /Users/Neo/Documents/git/PVTools/libs
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/Niuniu/libs
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/EXCHANGETOOLS/libs
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/EXCHANGETOOLS/libs
 cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AllEx/WebRoot/WEB-INF/lib
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AFASURL/ROOT/WEB-INF/lib
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/JEX/ROOT/WEB-INF/lib
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AFASURL/ROOT/WEB-INF/lib
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/JEX/ROOT/WEB-INF/lib
 cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AUTOWEIBO/libs
 mv easy.jar /Users/Neo/Documents/eclipse\ workspace/KejetExChange/lib
