4a89d3beed03e8ab5ae2130a501457cc80784138
Neo <starneo@gmail.com>
2016-04-06 15:32:33 +0800

    cpsql防止多线程

--- web/WEB-INF/classes/dabao.sh
+++ web/WEB-INF/classes/dabao.sh
@@ -1,13 +1,12 @@
-cd  /Users/Neo/Documents/git/EASYCORE/web/WEB-INF/classes/
+cd  /Users/Neo/git/EASYCORE/web/WEB-INF/classes/
 jar cvf easy.jar easy/*
 cp easy.jar /Users/Neo/Documents/svn/5pv/5PVREAL/libs
 cp easy.jar /Users/Neo/Documents/svn/91duan/Project/91DUANREAL/libs
 cp easy.jar /Users/Neo/Documents/git/TaoKe/libs
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/YOUHOW/web/WEB-INF/lib
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/STOCK/web/WEB-INF/lib
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/5PVSend/libs
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/YOUHOW/web/WEB-INF/lib
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/STOCK/web/WEB-INF/lib
+#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/5PVSend/libs
 cp easy.jar /Users/Neo/Documents/git/PVTools/libs
-cp easy.jar /Users/Neo/Documents/eclipse\ workspace/Niuniu/libs
 cp easy.jar /Users/Neo/Documents/eclipse\ workspace/EXCHANGETOOLS/libs
 cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AllEx/WebRoot/WEB-INF/lib
 cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AFASURL/ROOT/WEB-INF/lib
