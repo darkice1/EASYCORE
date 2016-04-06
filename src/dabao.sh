61531d14d23f849511489ac23dd8738f95bd6557
Neo <starneo@gmail.com>
2016-04-06 15:32:33 +0800

    cpsql防止多线程

--- src/dabao.sh
+++ src/dabao.sh
@@ -7,9 +7,9 @@ cp easy.jar /Users/Neo/Documents/git/TaoKe/libs
 #cp easy.jar /Users/Neo/Documents/eclipse\ workspace/STOCK/web/WEB-INF/lib
 #cp easy.jar /Users/Neo/Documents/eclipse\ workspace/5PVSend/libs
 cp easy.jar /Users/Neo/Documents/git/PVTools/libs
-#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/EXCHANGETOOLS/libs
+cp easy.jar /Users/Neo/Documents/eclipse\ workspace/EXCHANGETOOLS/libs
 cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AllEx/WebRoot/WEB-INF/lib
-#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AFASURL/ROOT/WEB-INF/lib
-#cp easy.jar /Users/Neo/Documents/eclipse\ workspace/JEX/ROOT/WEB-INF/lib
+cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AFASURL/ROOT/WEB-INF/lib
+cp easy.jar /Users/Neo/Documents/eclipse\ workspace/JEX/ROOT/WEB-INF/lib
 cp easy.jar /Users/Neo/Documents/eclipse\ workspace/AUTOWEIBO/libs
 mv easy.jar /Users/Neo/Documents/eclipse\ workspace/KejetExChange/lib
