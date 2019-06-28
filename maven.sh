4a2deb4bbd007d7cf9a79ef11a74cf8375760b70
Neo <darkice1@sina.com>
2019-06-28 10:31:07 +0800

    maven 更新脚本

--- maven.sh
+++ maven.sh
@@ -1,6 +1,19 @@
-mvn deploy -DaltDeploymentRepository=hengyunabc-mvn-repo::default::file:/Users/Neo/Documents/git/EASYLIB/repository
-cd /Users/Neo/Documents/git/EASYLIB/
-git update
+#!/usr/bin/env bash
+path=$(cd `dirname $0`; pwd)
+#echo $path
+
+lib="/Users/Neo/Documents/git/EASYLIB"
+rlib="$lib/repository"
+
+echo "更新 $lib"
+cd "$lib"
+git pull
+
+echo "打包 $lib"
+cd "$path"
+mvn deploy -DaltDeploymentRepository=hengyunabc-mvn-repo::default::file:$rlib
+
+cd $lib
 git add .
-git commit -m '$1'
+git commit -m "$1"
 git push
