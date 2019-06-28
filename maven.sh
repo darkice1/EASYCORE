547a9f702138f5cad9f1fdee74e9c96d7c53e1aa
Neo <darkice1@sina.com>
2019-06-28 10:03:01 +0800

    maven

--- maven.sh
+++ maven.sh
@@ -1,5 +1,6 @@
 mvn deploy -DaltDeploymentRepository=hengyunabc-mvn-repo::default::file:/Users/Neo/Documents/git/EASYLIB/repository
 cd /Users/Neo/Documents/git/EASYLIB/
+git update
 git add .
-git commit -m 'update'
+git commit -m '$1'
 git push
