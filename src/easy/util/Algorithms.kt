0862d6f4896fac3a610c23c3a76b75abce22c6c3
Neo <darkice1@sina.com>
2020-10-14 07:45:30 +0800

    更新二分法

--- src/easy/util/Algorithms.kt
+++ src/easy/util/Algorithms.kt
@@ -1,67 +1,43 @@
-package easy.util;
-
-import java.util.List;
+package easy.util
 
 /**
- * <p><i>Copyright: youhow.net(c) 2005-2011</i></p>
  *
- * �����㷨
+ * *Copyright: youhow.net(c) 2005-2011*
  *
- * @version 1.0 (<i>2011-5-27 neo(starneo@gmail.com)</i>)
+ * 常用算法类
+ *
+ * @version 1.0 (*2011-5-27 neo(starneo@gmail.com)*)
  */
-
-public class Algorithms
-{
+object Algorithms {
 	/**
-	 * ���ַ���������Ҫ���������
+	 * 二分法 放入数组需要先按顺序排训
 	 * @param <T>
 	 * @param list
 	 * @param o
 	 * @return
-	 */
-	public static <T extends Comparable<T>> T dichotomy(List<T> list,T o)
-	{
-//		List<T> list = new ArrayList<T>();
-//		list.addAll(l);
-//
-//		Collections.sort(list);
-		
-		int len = list.size()-1;
-		
-		int idx = len / 2;
-		int high = len;
-		int low = 0;
-
-		while (low <= high)
-		{
-			//System.out.print(idx+ " ");
-			T t = list.get(idx);
-			int re = t.compareTo(o);
-			if (re < 0)
-			{
-				low = idx+1;
+	</T> */
+	@JvmStatic
+	fun <T : Comparable<T>?> dichotomy(list: Array<T>, o: T): T? {
+		val len = list.size - 1
+		var idx = len / 2
+		var high = len
+		var low = 0
+		while (low <= high) {
+			val t = list[idx]
+			val re = t!!.compareTo(o)
+			when {
+				re < 0 -> {
+					low = idx + 1
+				}
+				re > 0 -> {
+					high = idx - 1
+				}
+				else -> {
+					return t
+				}
 			}
-			else if (re > 0)
-			{
-				high = idx-1;
-			}
-			else 
-			{
-				return t;
-			}
-			idx =(low+high)/2;
-			//System.out.println(low+" "+high+" "+idx);
-//			try
-//			{
-//				Thread.sleep(100);
-//			}
-//			catch(InterruptedException e)
-//			{
-//				e.printStackTrace();
-//			}
+			idx = (low + high) / 2
 		}
-		
-		
-		return null;
+		return null
 	}
 }
