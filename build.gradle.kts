55465a039ed2396e3eebbcd825e37bc60531e587
Neo <starneo@gmail.com>
2025-05-30 07:38:24 +0200

    更新版本至1.0.82，修复VulnerableLibrariesLocal抑制注释

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-//@file:Suppress("VulnerableLibrariesLocal")
+@file:Suppress("VulnerableLibrariesLocal")
 
 plugins {
 	kotlin("jvm") version "2.1.20"
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.github.darkice1"
-version = "1.0.81"
+version = "1.0.82"
 val projectName = "easy"
 val projectDesc = "Neo easy code."
 
